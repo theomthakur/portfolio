e850615894c54925056ec93de4b0170c842ee729
Om Thakur <thakurom0110@gmail.com>
2025-03-12 21:20:26 -0400

    unnecessary sections removed.

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,12 +1,11 @@
 import { FC } from "react";
 
-import { Box, Flex, Heading, Text, Image, Button, IconButton } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, Image } from "@chakra-ui/react";
 
 import { configs, Content, MarkdownFile, useContent } from "shared/content/Content";
 import { Education } from "pages/about/education/Education";
 import { Experience } from "pages/about/experience/Experience";
 import { Skills } from "pages/about/skills/Skills";
-import { VolumeIcon } from "utils/Icons";
 
 export const About: FC = () => {
     const content = useContent(MarkdownFile.About);
